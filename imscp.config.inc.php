fe670baeb578f52b79753209739d61fe0257795e
Laurent Declercq <l.declercq@nuxwin.com>
2013-11-06 09:37:52 +0100

    PhpMyAdmin addon: Small fix

--- imscp.config.inc.php
+++ imscp.config.inc.php
@@ -67,23 +67,24 @@ $cfg['Servers'][$i]['controlpass'] = '{PMA_PASS}';
 /**
  * Advanced phpMyAdmin features
  */
-//$cfg['Servers'][$i]['pmadb'] = 'phpmyadmin';
-//$cfg['Servers'][$i]['bookmarktable'] = 'pma_bookmark';
-//$cfg['Servers'][$i]['relation'] = 'pma_relation';
-//$cfg['Servers'][$i]['table_info'] = 'pma_table_info';
-//$cfg['Servers'][$i]['table_coords'] = 'pma_table_coords';
-//$cfg['Servers'][$i]['pdf_pages'] = 'pma_pdf_pages';
-//$cfg['Servers'][$i]['column_info'] = 'pma_column_info';
-//$cfg['Servers'][$i]['history'] = 'pma_history';
-//$cfg['Servers'][$i]['tracking'] = 'pma_tracking';
-//$cfg['Servers'][$i]['designer_coords'] = 'pma_designer_coords';
-//$cfg['Servers'][$i]['userconfig'] = 'pma_userconfig';
-//$cfg['Servers'][$i]['recent'] = 'pma_recent';
+$cfg['Servers'][$i]['pmadb']    = '{PMA_DATABASE}';
+$cfg['Servers'][$i]['bookmarktable'] = 'pma__bookmark';
+$cfg['Servers'][$i]['relation'] = 'pma__relation';
+$cfg['Servers'][$i]['table_info'] = 'pma__table_info';
+$cfg['Servers'][$i]['table_coords'] = 'pma__table_coords';
+$cfg['Servers'][$i]['pdf_pages'] = 'pma__pdf_pages';
+$cfg['Servers'][$i]['column_info'] = 'pma__column_info';
+$cfg['Servers'][$i]['history'] = 'pma__history';
+$cfg['Servers'][$i]['table_uiprefs'] = 'pma__table_uiprefs';
+$cfg['Servers'][$i]['tracking'] = 'pma__tracking';
+$cfg['Servers'][$i]['designer_coords'] = 'pma__designer_coords';
+$cfg['Servers'][$i]['userconfig'] = 'pma__userconfig';
+$cfg['Servers'][$i]['recent'] = 'pma__recent';
 
 /**
  * Hidden databases
  */
-$cfg['Servers'][$i]['hide_db'] = '(information_schema|performance_schema|phpmyadmin|mysql)';
+$cfg['Servers'][$i]['hide_db'] = '(information_schema|performance_schema|mysql)';
 
 /**
  * Disabling some warnings (disabled features, suhosin)
