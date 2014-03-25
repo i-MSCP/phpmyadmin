2cb132f457d071daf678737ba31d31156afd5011
Ninos Ego <me@ninosego.de>
2014-03-25 00:03:08 +0000

    Disable root access per default

--- imscp.config.inc.php
+++ imscp.config.inc.php
@@ -32,6 +32,7 @@ $i++;
  *Authentication type
  */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
+$cfg['Servers'][$i]['AllowRoot'] = false;
 
 /**
  * Parameters set by i-MSCP
@@ -48,7 +49,6 @@ $cfg['Servers'][$i]['connect_type'] = 'tcp';
 $cfg['Servers'][$i]['compress'] = true;
 $cfg['Servers'][$i]['extension'] = 'mysqli';
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
-$cfg['Servers'][$i]['AllowRoot'] = false;
 
 /**
  * rajk - for blobstreaming
