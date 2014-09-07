f6c99631600c47584436f77040883c4097cbed07
Laurent Declercq <l.declercq@nuxwin.com>
2014-09-07 17:50:32 +0200

    PhpMyAdmin addon: Updated to version 4.2.8.0

--- libraries/Config.class.php
+++ libraries/Config.class.php
@@ -114,7 +114,7 @@ class PMA_Config
      */
     function checkSystem()
     {
-        $this->set('PMA_VERSION', '4.2.7.1');
+        $this->set('PMA_VERSION', '4.2.8');
         /**
          * @deprecated
          */
